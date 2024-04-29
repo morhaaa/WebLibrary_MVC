00b3164d86a6e6e239033e2db06c2abc55604f5d
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-04-29 17:13:36 +0200

    feat(category): create category view and model

---
+++ WebLibrary/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebLibrary.Data;
+using WebLibrary.Models;
+
+namespace WebLibrary.Controllers{
+
+public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db){
+            _db = db;
+        }
+        public IActionResult Index()    {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList); 
+        }
+    }
+}
